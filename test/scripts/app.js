10a6431d00d61b4d4811b28ffeb8bb94489463c4
bladerunner2020 <pivovarov@gmail.com>
2019-01-05 13:49:40 +0300

    Fixed error with worng grunt contrib package

--- test/scripts/app.js
+++ test/scripts/app.js
@@ -1,4 +1,9 @@
+/* global IR */
 /* eslint-disable no-unused-vars */
+
 var appVersion = '{{ VERSION }}';
 var buildVersion = '{{ BUILD_VERSION }}';
 var fullVersion = '{{ VERSION }}-{{ BUILD_VERSION }}';
+
+IR.SetVariable('Global.AppVersion', appVersion);
+IR.SetVariable('Global.BuildVersion', buildVersion);
